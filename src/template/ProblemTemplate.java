162d26048e9e90032cc95724c3e9adba88f2d807
Ryan Knightly <reknightly@gmail.com>
2018-10-31 22:33:38 -0500

    Simplify ProblemTemplate

--- src/template/ProblemTemplate.java
+++ src/template/ProblemTemplate.java
@@ -2,37 +2,27 @@ package template;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import static java.lang.System.out; // allow use of out.println() without System
-
 
 public class ProblemTemplate {
 	// CHANGE FILE NAME FOR EACH PROBLEM!
-	private static final String inputFileName = "filename00.in";
-	private static Scanner scan;
+	private static final String inputFileName = "filename.in";
+	private static Scanner in;
 
-	public static void main(String[] args) {
-		try {
-			// Set up scanner to get input from file
-			scan = new Scanner(new File(inputFileName)); 
-			// Read the number of test cases from first line
-			int numCases = scan.nextInt();
-			// Read any other input variables at top of input file here
+	public static void main(String[] args) throws FileNotFoundException {
+		// Set up scanner to get input from file
+		in = new Scanner(new File(inputFileName));
+		// Read the number of test cases from first line
+		int numCases = in.nextInt();
+		// Read any other input variables at top of input file here
 
-			// Move to next line for test cases
-			scan.nextLine();
-			// Loop through each test case
-			for(int caseNum=0; caseNum<numCases; caseNum++) {
-				handleTestCase();
-			}
-			scan.close();
-		} catch (FileNotFoundException exception) {
-			exception.printStackTrace();
+		// Move to next line for test cases
+		in.nextLine();
+		// Loop through each test case
+		for(int caseNum=0; caseNum<numCases; caseNum++) {
+			// Replace this with test case handling
+			String currentLine = in.nextLine();
+			System.out.println(currentLine);
 		}
-	}
-
-	public static void handleTestCase() {
-		// Write solution for a single test case below
-		String inputLine = scan.nextLine();	// replace with custom input reading
-		out.println(inputLine);	// replace with custom input processing and output
+		in.close();
 	}
 }
